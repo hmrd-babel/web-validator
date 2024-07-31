9b7ffb137238b072c23f5fc63e74767786bf3643
Hugo Dias <hugo.dias@babelgroup.com>
2024-07-31 12:03:19 +0100

    testing vercel json;

--- check.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-      res.status(200).json({ message: 'Check successful' });
-    } else {
-      res.status(405).end(); // Method Not Allowed
-    }
-  }
