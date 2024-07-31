7046c719785ca4b26db08c6de383892c59dbabd2
Hugo Dias <hugo.dias@babelgroup.com>
2024-07-31 19:50:39 +0100

    last chance

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:3000', // Replace with your URL
 		video: false,
+		screenshotOnRunFailure: false
 	},
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,7 +6,7 @@ const { getAxeCorePath } = require('./path-helper');
 Cypress.Commands.add('injectAxe', () => {
   const axeCorePath = getAxeCorePath();
 
-  cy.readFile('node_modules/axe-core/axe.min.js').then((axeCoreJs) => {
+  cy.readFile('node_modules/axe-core/axe.min.js', { timeout: 10000 }).then((axeCoreJs) => {
     cy.window().then((win) => {
       // Inject the axe-core script into the page
       const script = win.document.createElement('script');
