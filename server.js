f0565cf87030b41892dda5c8a57774bad1a46009
Hugo Dias <hugo.dias@babelgroup.com>
2024-07-31 17:39:59 +0100

    testing AWS;

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cypress = require('cypress');
-const { exec } = require('child_process');
-
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,65 +23,39 @@ app.post('/check', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
 
 async function runCypressTest(url) {
+    const results = await cypress.run({
+        config: {
+            env: {
+                URL_TO_TEST: url,
+            },
+        },
+        spec: './cypress/e2e/accessibility.spec.cy.js',
+    });
+
     var toReturn = {
-        status: 200,
-        statusError: "",
-        violations: [],
-        totalRan: 0,
-        totalPassed: 0,
-        totalFailed: 0
+        violations : [],
+        totalRan : results.totalTests,
+        totalPassed : results.totalPassed,
+        totalFailed : results.totalFailed
     };
 
-    exec('npx cypress run', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          //return res.status(500).json({ error: error.message });
-          toReturn.status = 500;
-            toReturn.statusError = err.message;
-            return toReturn;
-        }
-    
-        res.json({ stdout, stderr });
-      });
+    if (results.totalFailed > 0) {
+        var errorsString = results.runs[0].tests[0].displayError;
+        var index = errorsString.lastIndexOf(']');
+        var cleaningString = errorsString.substring(0, index - 1);
+        var finalString = cleaningString.replace('Error:','');
+        finalString += "]";
 
-    try {
-        const results = await cypress.run({
-            config: {
-                env: {
-                    URL_TO_TEST: url,
-                },
-            },
-            spec: './cypress/e2e/accessibility.spec.cy.js',
-        });
-
-
-        toReturn.totalRan = results.totalTests;
-        toReturn.totalPassed = results.totalPassed;
-        toReturn.totalFailed = results.totalFailed;
+        toReturn.violations = JSON.parse(finalString);
         
-        if (results.totalFailed > 0) {
-            var errorsString = results.runs[0].tests[0].displayError;
-            var index = errorsString.lastIndexOf(']');
-            var cleaningString = errorsString.substring(0, index - 1);
-            var finalString = cleaningString.replace('Error:', '');
-            finalString += "]";
-
-
-            toReturn.violations = JSON.parse(finalString);
-
-            for (i = 0; i < toReturn.violations.length; i++) {
-                toReturn.violations[i].impactNumber = toReturn.violations[i].impact == 'critical' ? 0 : toReturn.violations[i].impact == 'serious' ? 1 : toReturn.violations[i].impact == 'moderate' ? 2 : 3;
-            }
-
-            return toReturn;
+        for (i=0; i< toReturn.violations.length; i++){
+            toReturn.violations[i].impactNumber =  toReturn.violations[i].impact == 'critical' ? 0 : toReturn.violations[i].impact == 'serious' ? 1 : toReturn.violations[i].impact == 'moderate' ? 2 : 3 ;
         }
-    } catch (err) {
-        toReturn.status = 500;
-        toReturn.statusError = err.message;
+
         return toReturn;
     }
 
