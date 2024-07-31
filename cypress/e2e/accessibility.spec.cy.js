1c50e769f27c316ecba88a5f2a60bbe7c7a99d49
Hugo Dias <hugo.dias@babelgroup.com>
2024-07-31 20:08:05 +0100

    checking

--- cypress/e2e/accessibility.spec.cy.js
+++ cypress/e2e/accessibility.spec.cy.js
@@ -1,13 +1,13 @@
 describe('Accessibility Validator', () => {
     beforeEach(() => {
         cy.visit(Cypress.env('URL_TO_TEST'));
-        cy.injectAxe();
+        cy.injectAxe({ axeCorePath: './node_modules/axe-core/axe.min.js'});
       });
 
     it('Validate accessibility', () => {
         cy.checkA11y(null, {
         }, (violations) => {
-            cy.task('log', violations); // Log the violations
+            //cy.task('log', violations); // Log the violations
             if (violations.length) {
                 throw new Error(JSON.stringify(violations, null, 2));
             }
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,19 @@
 import 'cypress-axe';
 
-// Import the helper file
-const { getAxeCorePath } = require('./path-helper');
+// // Import the helper file
+// const { getAxeCorePath } = require('./path-helper');
 
-Cypress.Commands.add('injectAxe', () => {
-  const axeCorePath = getAxeCorePath();
-  
-  console.log("callig injext Axe now");
+// Cypress.Commands.add('injectAxe', () => {
+//   const axeCorePath = getAxeCorePath();
 
-  cy.readFile('node_modules/axe-core/axe.min.js', { timeout: 10000 }).then((axeCoreJs) => {
-    cy.window().then((win) => {
-      // Inject the axe-core script into the page
-      const script = win.document.createElement('script');
-      script.innerHTML = axeCoreJs;
-      win.document.head.appendChild(script);
-    });
-  });
-});
+//   console.log("callig injext Axe now");
+
+//   cy.readFile('node_modules/axe-core/axe.min.js', { timeout: 10000 }).then((axeCoreJs) => {
+//     cy.window().then((win) => {
+//       // Inject the axe-core script into the page
+//       const script = win.document.createElement('script');
+//       script.innerHTML = axeCoreJs;
+//       win.document.head.appendChild(script);
+//     });
+//   });
+// });
