71225ca8f0828f39d7ef03cb4d74ec08550844e2
Hugo Dias <hugo.dias@babelgroup.com>
2024-08-01 16:48:27 +0100

    preparing production and development enviornments;

--- cypress/e2e/accessibility.spec.cy.js
+++ cypress/e2e/accessibility.spec.cy.js
@@ -1,7 +1,11 @@
+const path = require('path');
+// Access the file path based on the environment
+const filePath = process.env.FILE_PATH || './node_modules/axe-core/axe.min.js';
+
 describe('Accessibility Validator', () => {
     beforeEach(() => {
         cy.visit(Cypress.env('URL_TO_TEST'));
-        cy.injectAxe({ axeCorePath: './node_modules/axe-core/axe.min.js'});
+        cy.injectAxe({ axeCorePath: filePath});
       });
 
     it('Validate accessibility', () => {
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
