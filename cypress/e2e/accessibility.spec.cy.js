c75247676d17e5bf109ad6e08a492ba4471b3499
Hugo Dias <hugo.dias@babelgroup.com>
2024-08-01 18:44:27 +0100

    made a mistake

--- cypress/e2e/accessibility.spec.cy.js
+++ cypress/e2e/accessibility.spec.cy.js
@@ -5,7 +5,7 @@ const filePath = process.env.FILE_PATH || '../node_modules/axe-core/axe.min.js';
 describe('Accessibility Validator', () => {
     beforeEach(() => {
         cy.visit(Cypress.env('URL_TO_TEST'));
-        cy.injectAxe({ axeCorePath: '../node_modules/axe-core/axe.min.js'});
+        cy.injectAxe({ axeCorePath: './node_modules/axe-core/axe.min.js'});
       });
 
     it('Validate accessibility', () => {
